e7d2000e33a022e15f21f152673ab56c4d3852b9
Daavide <d.magni96@gmail.com>
2018-02-12 00:25:48 +0100

    First Fix

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
 #Raspy install script
 
+echo "Checking if root..."
+if [[ $(id -u) -ne 0 ]] ; then echo "Not root. Please fix." ; exit 1 ; fi
 
 echo "Installing packages.."
 
 apt-get update
 apt-get -y upgrade
 apt-get -y install hostapd tcptrack htop nbtscan vnstat
+
+echo "Enabling routing..."
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+echo "Enabling NAT..."
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables-save > /etc/iptables.ipv4.nat
+
+apt-get -y install iptables-persistent
+
+echo "Installing Pi-Hole..."
+
 curl -sSL https://install.pi-hole.net | bash
 
 echo "Insert SSID: "
@@ -13,6 +31,19 @@ read -sr SSID
 
 echo "Insert password: "
 read -sr PASSWORD
+
+echo "Insert Pi-Hole WEB-GUI password (http://raspberry-ip/admin): "
+
+pihole -a -p 
+
+"Finishing up..."
+
+service hostapd enable
+service iptables-persistent enable
+
+"Done!"
+
+
 
 #Hostapd conf file:
 
@@ -31,6 +62,25 @@ rsn_pairwise=CCMP
 ctrl_interface=/var/run/hostapd
 ctrl_interface_group=0
 
+
+
+#Commands
+
+raspy connected_clients
+  hostapd_cli all_sta | grep : | grep -v dot
+  arp -a | grep -station-
+  grep -first_mac_address_half- /usr/share/nmap/nmap-mac-prefixes
+
+raspy performance
+
+raspy traffic_usage
+
+raspy monitor
+
+raspy deauth
+
+#script
+#<>
 
 
 
--- raspy.py
+++ raspy.py
@@ -5,10 +5,21 @@ import os
 
 #check args
 
+
 print "Number of arguments: ", len(sys.argv)
 print "The arguments are: " , str(sys.argv)
 
-arg = sys.argv[1]
+if len(sys.argv) == 1:
+  print """
+  Raspy CLI Utility. Available commands:
+    raspy connected_clients: see info about hosts connected to WiFi.
+    raspy performance: see real time CPU and RAM info. Powered by htop.
+    raspy traffic_usage: see traffic amount on each interface since activation Powered by vnstat.
+    raspy monitor: see who is using bandwidth. Powered by tcptrack.
+    raspy deauth: disconnect a client from WiFi.
+  """
+else:
+  arg = sys.argv[1]
 
 if len(sys.argv) > 2:
   print "Too much arguments."
@@ -26,10 +37,11 @@ if arg == "help":
 elif arg == "connected_clients":
   print "Connected Clients:"
   macs = os.popen("hostapd_cli all_sta | grep : | grep -v dot").read().split()
-  for mac in macs:
-    print os.popen("arp -a | grep " + mac).read().split()[0]
-    mac_split = mac.split(":")
-    print os.popen("grep -i " mac_split[0] + mac_split[1] + mac_split[2] " /usr/share/nmap/nmap-mac-prefixes").read()
-    print ""
-
+  if macs == []:
+    print "Are you root?"
+  else:
+    for mac in macs:
+      print os.popen("arp -a | grep " + mac).read().split("at")[0]
+      mac_split = mac.split(":")
+      print os.popen("grep -i " + mac_split[0] + mac_split[1] + mac_split[2] + " /usr/share/nmap/nmap-mac-prefixes").re$
 
